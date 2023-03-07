d825dc814936c85ac3feabc69b65173a76b56a33
Shiv Kushwah <shivkushwah@fb.com>
2023-03-06 21:13:29 -0800

    create playlist

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
+import json
+
+PLAYLIST_TO_BE_GENERATED_NAME = 'dev'
 
 # Obtain an oauth token by going to https://developer.spotify.com/console/get-search-item/ and pressing "Get Token"
-
 # Save this token in a file called oauthtoken.txt
 with open('oauthtoken.txt') as f:
     token = f.readlines()[0].strip()
+    bearer_token = f'Bearer {token}'
 
 # Obtain Track IDs for each song track in the text file
 # Obtain each song track in the text file
@@ -20,13 +23,25 @@ search_endpoint_url = 'https://api.spotify.com/v1/search?'
 limit = 1
 market = 'US'
 query_type = 'track'
+spotify_user_id = 'shivkushwah'
 
 for song in songs:
     search_query = f'{search_endpoint_url}q={song}&type={query_type}&limit={limit}&market={market}'
     response =requests.get(search_query,
                 headers={"Content-Type":"application/json",
-                            "Authorization":f'Bearer {token}'})
+                            "Authorization":bearer_token})
     for item in response.json()['tracks']['items']:
         song_ids.append(item['id'])
 
-print(song_ids)
+# Create new playlist
+playlist_endpoint_url = f'https://api.spotify.com/v1/users/{spotify_user_id}/playlists'
+request_body = json.dumps({
+          "name": PLAYLIST_TO_BE_GENERATED_NAME,
+          "description": "dev time",
+          "public": True
+        })
+
+response = requests.post(url = playlist_endpoint_url, data = request_body, headers={"Content-Type":"application/json",
+                        "Authorization":bearer_token})
+playlist_id = response.json()['id']
+print(playlist_id)
